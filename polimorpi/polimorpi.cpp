8819ee32c6d70e42cd352b580c06f5f6ca86e5c3
marchdillo <kevinno.limiy@gmail.com>
2025-06-02 16:24:26 +0700

    menambahkan polimorpi

---
+++ polimorpi/polimorpi.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class baseClass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hello saya Function dari base class";
+    }
+};
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hello saya Function dari derived Class";
+    }
+};
+
+int main()
+{
+    derivedClass a;
+
+    a.perkenalan();
+
+    return 0;
+};
